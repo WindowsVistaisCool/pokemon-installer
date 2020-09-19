59f09cd081d55131df570c2cdbcc36ec0a23aef1
KYle Rush <57149988+WindowsVistaisCool@users.noreply.github.com>
2020-09-18 20:46:08 -0400

    color class

---
+++ USTRINGS.py
@@ -0,0 +1,18 @@
+# Why is this file called USTRINGS?
+# cuz i was too lazy to come up with a good name lol
+
+# learn your colors again children
+class bcolors:
+  HEADERS = '\033[95m'
+  OKBLUE = '\033[94m'
+  OKGREEN = '\033[92m'
+  WARNING = '\033[93m'
+  FAIL = '\033[91m'
+  ENDC = '\033[0m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+
+#other files will call this for readme hehe
+def readme():
+  # RETURN SUPER DUPER EXTRA DUCKING LONG STRING HAHAHAHAHAHAHAAHAH
+ 
