548e1fb3a79b353a6e625471ece4c2ada4798b16
KYle Rush <57149988+WindowsVistaisCool@users.noreply.github.com>
2020-09-18 20:07:45 -0400

    clictrl for dev mode on/off and cloning options

---
+++ clictrl.py
@@ -0,0 +1,16 @@
+dev_mode = False
+enable_clone = False
+enable_rgbds = False
+enable_redblue = False
+enable_goldsilver = False
+
+def secrets():
+  return dev_mode
+def allow_clone():
+  return enable_clone
+def allow_rgbds():
+  return enable_rgbds
+def allow_redblue():
+  return enable_redblue
+def allow_goldsilver():
+  return enable_goldsilver
